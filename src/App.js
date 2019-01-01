11b64ff69c44e2f58c822cf96716c7c1b05c6245
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-01 20:00:59 +1100

    added state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Mahir', age: 20 },
+      { name: 'Meherwan', age: 19 },
+      { name: 'Marcel', age: 21 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Learn React</h1>
-        <Person name="Mahir" age="20"></Person>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+          Hobbies: sleeping
+        </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
   }
