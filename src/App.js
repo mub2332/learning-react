c99b3b170313ba572c23eaf791a0c27b4fe45eff
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-02 13:44:44 +1100

    added dynamic update to all list items

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Mahir', age: 20 },
-      { name: 'Meherwan', age: 19 },
-      { name: 'Marcel', age: 21 }
+      { id: '1', name: 'Mahir', age: 20 },
+      { id: '2', name: 'Meherwan', age: 19 },
+      { id: '3', name: 'Marcel', age: 21 }
     ],
     showPersons: false
   };
@@ -21,13 +21,21 @@ class App extends Component {
     });
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({ 
-      persons: [
-        { name: event.target.value, age: 20 },
-        { name: 'Meherwan', age: 19 },
-        { name: 'Marcel', age: 21 }
-      ]
+      persons: persons
     });
   }
 
@@ -52,9 +60,11 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return <Person 
-              click={this.deletePersonHandler.bind(this, index)}
               name={person.name}
-              age={person.age}></Person>
+              age={person.age}
+              key={person.id}
+              click={this.deletePersonHandler.bind(this, index)}
+              changed={(event) => this.nameChangedHandler(event, person.id)}></Person>
           })}
         </div>
       );
